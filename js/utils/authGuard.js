daca5da66a7122f924d679122d6a0a7f506efa00
Ricardo <ricar.arturo@gmail.com>
2025-09-15 19:26:10 -0600

    AuthGuard.js eliminado

--- js/utils/authGuard.js
+++
@@ -1,9 +0,0 @@
-import { me } from "../services/authService.js";
-(async function () {
-  try {
-    const info = await me();
-    if (!info?.authenticated) window.location.href = "login.html";
-  } catch {
-    window.location.href = "login.html";
-  }
-})();
